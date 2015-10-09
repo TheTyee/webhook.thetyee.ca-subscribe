d4cc8ee61d9d660ed987a3691d58a79adfe6ff64
Phillip Smith <ps@phillipadsmith.com>
2015-10-08 17:35:39 -0700

    First commit of subscription micro app

---
+++ app.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite;
+use Mojo::Util qw( url_escape );
+use Try::Tiny;
+use Data::Dumper;
+
+
+# Get the configuration
+my $config = plugin 'JSONConfig';
+app->secrets( [ $config->{'app_secret'} ] );
+
+# Get a UserAgent
+my $ua = Mojo::UserAgent->new;
+
+# WhatCounts setup
+my $API        = $config->{'wc_api_url'};
+my $wc_list_id = $config->{'wc_listid'};
+my $wc_realm   = $config->{'wc_realm'};
+my $wc_pw      = $config->{'wc_password'};
+my $secret     = $config->{'post_secret'};
+my $host       = $config->{'host'};
+
+post '/' => sub {
+    my $c = shift;
+    # Only accept POSTS from a specified host
+    unless ( $c->req->content->headers->host =~ $host ) {
+        $c->render( text => "Forbidden", status => 403 );
+        return;
+    }
+
+    # Grab the post data
+    my $email       = $c->param( 'email' ) || '';
+    my $campaign    = url_escape $c->param( 'campaign' ) || '';
+    my $frequency   = $c->param( 'frequency' ) || '';
+    # Post it to WhatCounts
+    my $args = {
+        r                     => $wc_realm,
+        p                     => $wc_pw,
+        list_id               => $wc_list_id,
+        cmd                   => 'sub',
+        override_confirmation => '1',
+        force_sub             => '1',
+        format                => '2',
+        data =>
+            "email,custom_campaign,custom_pref_enews_$frequency,custom_pref_tyeenews_casl^$email,$campaign,1,1"
+    };
+    # Output $args when debugging
+    app->log->debug( Dumper( $args ) );
+    my $result;
+    my $tx = $ua->post( $API => form => $args );
+    if ( my $res = $tx->success ) {
+        $result = $res->body;
+        # Output response when debugging
+        app->log->debug( Dumper( $result ) );
+        # Send 200 back to the request
+        $c->render( text => "$result", status => 200 );
+    }
+    else {
+        my ( $err, $code ) = $tx->error;
+        $result = $code ? "$code response: $err" : "Connection error: $err";
+        # TODO this needs to notify us of a problem
+        app->log->debug( Dumper( $result ) );
+        # Send a 500 back to the request
+        $c->render( text => "$result", status => 500 );
+    }
+};
+
+app->start;
