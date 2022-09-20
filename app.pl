0db89f1c8c274590d95edd02713df94fd6fa110e
mrbryan <tyeeapps@tyeeweb.nmsrv.com>
2022-09-20 16:40:33 -0700

    targetting mailchimp

--- app.pl
+++ app.pl
@@ -3,6 +3,9 @@ use Mojolicious::Lite;
 use Mojo::Util qw( url_escape );
 use Try::Tiny;
 use Data::Dumper;
+use JSON qw(encode_json decode_json);
+sub true () { JSON::true }
+sub false () { JSON::false }
 
 
 # Get the configuration
@@ -47,34 +50,60 @@ helper get_subscriber_id => sub {
 post '/' => sub {
     my $c = shift;
     # Only accept POSTS from a specified host
-    unless ( $c->req->content->headers->host =~ $host ) {
-        $c->render( text => "Forbidden", status => 403 );
-        return;
-    }
-    $c->res->headers->header('Access-Control-Allow-Origin' => '*');
+   # unless ( $c->req->content->headers->host =~ $host ) {
+    #    $c->render( text => "Forbidden", status => 403 );
+     #   return;
+   # }
+   
+   
+   $c->res->headers->header('Access-Control-Allow-Origin' => '*'); 
+    $c->res->headers->access_control_allow_origin('*');
+    $c->res->headers->header('Access-Control-Allow-Methods' => 'GET, OPTIONS, POST, DELETE, PUT');
+    $c->res->headers->header('Access-Control-Allow-Headers' => 'Content-Type' => 'application/x-www-form-urlencoded');
+   
+   #$c->res->headers->header('Access-Control-Allow-Origin' => 'https://preview.thetyee.ca');
+   
 
     # Grab the post data
     my $email       = $c->param( 'email' )                      || '';
     my $campaign    = url_escape $c->param( 'custom_campaign' ) || '';
     my $frequency   = $c->param( 'frequency' ) || '';
-    my $national    = $c->param( 'custom_pref_enews_national' );
-    my $weekly      = $c->param( 'custom_pref_enews_weekly' );
-    my $daily       = $c->param( 'custom_pref_enews_daily' );
-    my $events       = $c->param( 'custom_pref_enews_events' );
-    my $whitegaze   =  $c->param( 'custom_pref_enews_white_gaze' );
+    my $national    = $c->param( 'custom_pref_enews_national' ) || '';
+    my $weekly      = $c->param( 'custom_pref_enews_weekly' ) || '';
+    my $daily       = $c->param( 'custom_pref_enews_daily' ) || '';
+    my $events       = $c->param( 'custom_pref_enews_events' ) || '';
+    my $whitegaze   =  $c->param( 'custom_pref_enews_white_gaze' ) || '';
 
 my $ub = Mojo::UserAgent->new;
 
-    # Post it to WhatCounts
+my $merge_fields = {
+    P_S_CASL => 1,
+    CAMPAIGN => $campaign,
+    P_T_CASL => 1,
+    P_E_NAT => $national,
+    P_E_WEEKLY => $weekly,
+    P_E_DAILY => $daily,
+    P_E_EVENTS => $events,
+    P_E_W_GAZE => $whitegaze
+};
+    
+my $interests = {};
+
+if ($national) { $interests -> {'34d456542c'} = \1 };
+if ($daily) { $interests -> {'e96d6919a3'} = \1 };
+if ($weekly) {$interests -> {'7056e4ff8d'} = \1 };
+
+    # Post it to Mailchimp
     my $args = {
-        r                     => $wc_realm,
-        p                     => $wc_pw,
-        list_id               => $wc_list_id,
-        cmd                   => 'sub',
-        override_confirmation => '1',
-        force_sub             => '1',
-        format                => '2',
-        data =>"email,custom_pref_sponsor_enews,custom_pref_sponsor_casl,custom_campaign,custom_pref_tyeenews_casl,custom_pref_enews_$frequency,custom_pref_enews_national,custom_pref_enews_weekly,custom_pref_enews_daily,custom_pref_enews_events,custom_pref_enews_white_gaze^$email,1,1,$campaign,1,1,$national,$weekly,$daily,$events,$whitegaze"};
+        email_address   => $email,
+        status =>       => 'subscribed',
+        merge_fields => $merge_fields,
+        interests => $interests
+    };
+        
+        
+       
+     #   data =>"email,custom_pref_sponsor_enews,custom_pref_sponsor_casl,custom_campaign,custom_pref_tyeenews_casl,custom_pref_enews_$frequency,custom_pref_enews_national,custom_pref_enews_weekly,custom_pref_enews_daily,custom_pref_enews_events,custom_pref_enews_white_gaze^$email,1,1,$campaign,1,1,$national,$weekly,$daily,$events,$whitegaze"};
     # Output $args when debugging
     app->log->debug( Dumper( $args ) );
 
@@ -87,13 +116,22 @@ my $ub = Mojo::UserAgent->new;
     my $errorHtml = '<p>' . $errorText . '</p>';
     my $notification;
     my $result;
-    my $tx = $ua->post( $API => form => $args );
-    if ( my $res = $tx->success ) {
-        $result = $res->body;
+    my $URL = Mojo::URL->new('https://Bryan:' . $config->{"mc_key"} . '@us14.api.mailchimp.com/3.0/lists/' . $config->{"mc_listid"} . '/members');
+    my $tx = $ua->post( $URL => json => $args );
+    my $js = $tx->result->json;
+     app->log->debug( "code" . $tx->res->code);
+      app->log->debug( Dumper( $js));
+     app->log->debug( "unique email id" .  $js->{'unique_email_id'});
+  
+# check params at https://docs.mojolicious.org/Mojo/Transaction/HTTP
+  
+    if ($tx->res->code == 200 ) {
+        $result = $tx->result->body;
         # Output response when debugging
-        app->log->debug( Dumper( $result ) );
-        if ( $result =~ 'SUCCESS' ) {
-            my $subscriberId = $c->get_subscriber_id( $email );
+      #          app->log->debug( Dumper( $tx  ) );
+      #  app->log->debug( Dumper( $result ) );
+        if ( $result =~ 'subscribed' ) {
+            my $subscriberId = $js->{'unique_email_id'};
             # Send 200 back to the request
             $c->render( json => { 
                     text => $successText, 
@@ -127,6 +165,8 @@ $ub->post($config->{'notify_url'} => json => {text => $notification }) unless $e
                     resultStr => $result }, 
                 status => 500 );
 	app->log->info("error: "  . $errorText) unless $email eq 'api@thetyee.ca';
+            app->log->debug("error: "  . $errorText);
+
     }
 };
 
